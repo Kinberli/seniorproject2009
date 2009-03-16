28328ec6ffdb993347e4331636186d033221141d
Negonicrac <negonicrac@gmail.com>
2009-03-15 21:49:01 -0400

    initial commit

---
+++ SeniorProject2009/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SeniorProject2009
+{
+    public class Class1
+    {
+        /* This is an empty project an at this time serves no purpose. */
+    }
+}
